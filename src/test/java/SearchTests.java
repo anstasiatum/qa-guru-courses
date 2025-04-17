215edd26faaba8715e847758482160e13cc5d64f
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-04-17 19:34:46 +0300

    initial commit

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,14 +1,42 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests {
-    @Test
-    void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    public class SearchTests {
+
+        //Yandex main page opened
+        @Test
+        void yandexSuccessfulSearchTest() {
+            Configuration.holdBrowserOpen = true;
+            open("https://ya.ru/");
+            $("html").shouldHave(text("Войти"));
+        }
+
+        //Google main page opened
+        @Test
+        void googlePageCanBeOpened() {
+            Configuration.holdBrowserOpen = true;
+            open("https://www.google.com/");
+            $("html").shouldHave(text("Россия"));
+        }
+
+        //DuckDuckGo main page opened
+        @Test
+        void duckDuckGoPageCanBeOpened() {
+            Configuration.holdBrowserOpen = true;
+            open("https://duckduckgo.com/");
+            $("html").shouldHave(text("Chat"));
+        }
+
+        //DuckDuckGo search results
+        @Test
+        void duckDuckGoSuccessfulSearchTest() {
+            open("https://duckduckgo.com/");
+            $("#searchbox_input").setValue("selenide").pressEnter();
+            $("#web_content_wrapper").shouldHave(text("https://selenide.org"));
+        }
     }
-}
+
